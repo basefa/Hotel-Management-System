b85e399ad22bbb0900a7f1ec12ed387be72c5ebe
Bruck Asefa <basefa1@umbc.edu>
2024-11-04 18:01:24 -0500

    Initial commit START OF THE RESERVATION

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main (String [] args) {
+
+
+        System.out.println(" Thank you for choosing our management system");
+
+
+
+    }
+}
---
+++ src/TypeOfRoom.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TypeOfRoom {
+
+
+
+    private static String single;
+    private static String double;
+    private static String queen;
+    private static String king;
+    private static String suite;
+
+
+
+    public static void getRoomType() {
+
+        System.out.println(" Please select the room you would like to reserve");
+        System.out.println("\t*MENU");
+        System.out.println(" Single");
+        System.out.println(" Double");
+        System.out.println(" Queen");
+        System.out.println(" King");
+        System.out.println(" Suite");
+
+        Scanner sc = new Scanner(System.in);
+        String roomtype = sc.nextLine();
+
+        if (roomtype == single){
+
+            System.out.println(" You have selected a single room");
+
+        } else if (roomtype== double) {
+            
+        }
+    }
+}
