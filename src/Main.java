42acba2b303642126de2d40a6e3c080ef45cb572
Sarah <s362@umbc.edu>
2024-11-18 17:41:49 -0500

    Tweaked some thangs
    (cherry picked from commit b3be820a69f12cf3e12e04fff44f641bbf0b83b6)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-
+import java.util.Scanner;
 public class Main {
+    static Scanner scanner= new Scanner(System.in);
     public static void main (String [] args) {
-
-        System.out.println(" Thank you for choosing our management system");
-
-        TypeOfRoom.getRoomType();
-
+    }
 
     }
-}
