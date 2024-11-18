c9ac4ab207bad704d7d697948ac3a88e12dc3a57
basefa <46939663+basefa@users.noreply.github.com>
2024-11-18 18:01:14 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,19 @@ public class Main {
     static Scanner scanner= new Scanner(System.in);
     public static void main (String [] args) {
     }
+      TypeOfRoom.getRoomType();
+
+
+        //Scanner scanner = new Scanner(System.in);
+
+        // Ask for room type and days stayed
+        //System.out.print("Enter room type (Single/Double/Queen/King/Suite): ");
+       // String roomType = scanner.nextLine();
+        //System.out.print("Enter number of days stayed: ");
+        //int daysStayed = scanner.nextInt();
+
+        // Calculate and display the bill
+        //Billing.calculateBill(roomType, Billing.daysStayed);
+    }
 
     }
