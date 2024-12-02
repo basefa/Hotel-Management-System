9dd89e4d11496c86573f048ff689d6d376001635
Sarah <s362@umbc.edu>
2024-12-02 17:36:41 -0500

    Updated the main

--- src/Activity.java
+++ src/Activity.java
@@ -1,16 +1,16 @@
 public class Activity {
 
-public static void activity(String[] args) {
+    public static void activity(String[] args) {
 
-    System.out.println(" Here are our weekly events if you would like make reservation. Please select one of our events");
-    System.out.println(" Morning yoga on Sunday at 10 AM - 12 AM");
-    System.out.println(" Food truck in front of the hotel on Fridays  2 PM - 6 PM ");
-    System.out.println(" Cooking class on Wednesdays 12 PM - 2 PM");
-    System.out.println(" Discount Spa on Saturdays from 8 AM - 10 AM");
-    System.out.println(" Live music at the ball room on Thursdays 6 PM - 9 PM");
-
-
+        System.out.println(" Here are our weekly events if you would like make reservation. Please select one of our events");
+        System.out.println(" Morning yoga on Sunday at 10 AM - 12 AM");
+        System.out.println(" Food truck in front of the hotel on Fridays  2 PM - 6 PM ");
+        System.out.println(" Cooking class on Wednesdays 12 PM - 2 PM");
+        System.out.println(" Discount Spa on Saturdays from 8 AM - 10 AM");
+        System.out.println(" Live music at the ball room on Thursdays 6 PM - 9 PM");
 
 
- }
+
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,35 @@ public class Main {
         System.out.println();
 
 
-        TypeOfRoom.getRoomType();
+
         //Prompt user for guest info
-        System.out.println ("Please enter your Guest ID, Email, or Full Name:");
-        String guestInfo=scanner.nextLine();
+        System.out.println("Please enter your Guest ID: ");
+        String guestID = scanner.nextLine();
+        System.out.println("Please enter your Full Name: ");
+        String guestName = scanner.nextLine();
+        System.out.println("Please enter your Email: ");
+        String email = scanner.nextLine();
 
+        // Create Guest_info object and display the info
+        Guest_info guest = new Guest_info();
+        guest.GuestInfo(guestID, guestName, email);
+        guest.displayInfo();
 
+        // Select Room Type
+        TypeOfRoom.getRoomType();  // Allows user to select room type and quantity
 
+        // Call activity method to event schedule
+        Activity.activity(null);
+
+        // 4. Get Room Type
+        System.out.println("Enter room type for billing (Single/Double/Queen/King/Suite): ");
+        String roomType = scanner.nextLine();
+        System.out.println("Enter number of days stayed: ");
+        int daysStayed = scanner.nextInt();
+
+        //Calculate the total Bill
+        Billing.calculateBill(roomType, daysStayed);  // Call Billing to calculate and display the bill
+    }
 
     }
-      TypeOfRoom.getRoomType();
 
-
-        //Scanner scanner = new Scanner(System.in);
-
-        // Ask for room type and days stayed
-        //System.out.print("Enter room type (Single/Double/Queen/King/Suite): ");
-       // String roomType = scanner.nextLine();
-        //System.out.print("Enter number of days stayed: ");
-        //int daysStayed = scanner.nextInt();
-
-        // Calculate and display the bill
-        //Billing.calculateBill(roomType, Billing.daysStayed);
-    }
-
-    }
